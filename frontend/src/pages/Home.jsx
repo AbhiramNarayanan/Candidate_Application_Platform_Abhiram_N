20e9fbf93ddc390f04e3ee14795b6a41d6b0bedf
Abhiram N <narayananabhiram@gmail.com>
2024-05-06 15:37:38 +0530

    filtering of job cards by using roles, min exp, location, work type, min base salary, also the search functionality using company name, role or location completed... Project completed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,6 +7,10 @@ const Home = ({ url, limit = 5 }) => {
   const [jobs, setJobs] = useState([]);
   const [search, setSearch] = useState("");
   const [selectedRole, setSelectedRole] = useState("");
+  const [selectedMinExp, setSelectedMinExp] = useState("");
+  const [selectedLocation, setSelectedLocation] = useState("");
+  const [selectedWorkType, setSelectedWorkType] = useState("");
+  const [selectedMinSalary, setSelectedMinSalary] = useState(""); 
   const [loading, setLoading] = useState(false);
   const [errorMsg, setErrorMsg] = useState(null);
   const [reachedEnd, setReachedEnd] = useState(false); //state to track if the end is reached
@@ -42,10 +46,10 @@ const Home = ({ url, limit = 5 }) => {
   }
 
   useEffect(() => {
-    if (url !== "" && selectedRole === "") {
+    if (url !== "" && selectedRole === "" && selectedMinExp === "" && selectedLocation === "" && selectedWorkType === "" && selectedMinSalary === "") {
       fetchJobs(url);
     }
-  }, [url, selectedRole]);
+  }, [url, selectedRole, selectedMinExp, selectedLocation, selectedWorkType, selectedMinSalary]);
 
   function handleScrollPercentage() {
     const scrolledToBottom =
@@ -67,7 +71,6 @@ const Home = ({ url, limit = 5 }) => {
   }, [loading, reachedEnd]);
 
 
-
   const searchJobs = jobs.filter((job) => {
     return Object.keys(job).some((key) =>
       job[key] &&
@@ -75,11 +78,20 @@ const Home = ({ url, limit = 5 }) => {
         .toString()
         .toLowerCase()
         .includes(search.toString().toLowerCase())
-    ) && (selectedRole === "" || job.jobRole === selectedRole); 
+    ) && (selectedRole === "" || job.jobRole === selectedRole) && (selectedMinExp === "" || job.minExp >= selectedMinExp) && (selectedLocation === "" || job.location === selectedLocation) && (selectedWorkType === "" || (selectedWorkType === "Remote" ? job.location.toLowerCase() === "remote" : job.location.toLowerCase() !== "remote")) && (selectedMinSalary === "" || job.minJdSalary >= selectedMinSalary); 
   });
 
   // Get all unique roles in ascending order
   const roles = [...new Set(jobs.map((job) => job.jobRole))].sort();
+
+  // Get all unique minimum experiences in ascending order
+  const minExperiences = [...new Set(jobs.map((job) => job.minExp))].sort((a, b) => a - b);
+
+  // Get all unique locations in ascending order
+  const locations = [...new Set(jobs.map((job) => job.location))].sort();
+
+  // Get all unique minimum base pay salaries in ascending order
+  const minSalaries = [...new Set(jobs.map((job) => job.minJdSalary))].sort((a, b) => a - b);
   
 
 
@@ -92,13 +104,46 @@ const Home = ({ url, limit = 5 }) => {
 
   return (
     <div style={{ padding: "1rem" }}>
-    <div style={{marginLeft:"1rem"}}>
+    <div style={{marginLeft:"1rem", marginBottom:"1.3rem"}}>
 
-    <select value={selectedRole} onChange={(e) => setSelectedRole(e.target.value)} style={{width:"10rem", height:"2.8rem", borderRadius:"0.5rem", border:"2px solid #d9d9d9", paddingLeft:"1rem", color:"gray"}}>
+    <select value={selectedRole} onChange={(e) => setSelectedRole(e.target.value)} style={{width:"12rem", height:"2.8rem", borderRadius:"0.5rem", border:"2px solid #d9d9d9", paddingLeft:"1rem", color:"gray"}}>
           <option value="">Roles</option>
           {roles.map((role) => (
             <option key={role} value={role}>
               {role}
+            </option>
+          ))}
+        </select>
+
+        <select value={selectedMinExp} onChange={(e) => setSelectedMinExp(e.target.value)} style={{width:"12rem", height:"2.8rem", borderRadius:"0.5rem", border:"2px solid #d9d9d9", paddingLeft:"1rem", color:"gray", marginLeft:"0.8rem"}}>
+          <option value="">Min Experience</option>
+          {minExperiences.map((minExp) => (
+            <option key={minExp} value={minExp}>
+              {minExp}
+            </option>
+          ))}
+        </select>
+
+        <select value={selectedLocation} onChange={(e) => setSelectedLocation(e.target.value)} style={{width:"12rem", height:"2.8rem", borderRadius:"0.5rem", border:"2px solid #d9d9d9", paddingLeft:"1rem", color:"gray", marginLeft:"0.8rem"}}>
+          <option value="">Location</option>
+          {locations.map((location) => (
+            <option key={location} value={location}>
+              {location}
+            </option>
+          ))}
+        </select>
+
+        <select value={selectedWorkType} onChange={(e) => setSelectedWorkType(e.target.value)} style={{width:"12rem", height:"2.8rem", borderRadius:"0.5rem", border:"2px solid #d9d9d9", paddingLeft:"1rem", color:"gray", marginLeft:"0.8rem"}}>
+          <option value="">Work Type</option>
+          <option value="Remote">Remote</option>
+          <option value="Onsite">Onsite</option>
+        </select>
+
+        <select value={selectedMinSalary} onChange={(e) => setSelectedMinSalary(e.target.value)} style={{width:"13rem", height:"2.8rem", borderRadius:"0.5rem", border:"2px solid #d9d9d9", paddingLeft:"1rem", color:"gray", marginLeft:"0.8rem"}}>
+          <option value="">Min Base Pay Salary</option>
+          {minSalaries.map((minSalary) => (
+            <option key={minSalary} value={minSalary}>
+              {minSalary}
             </option>
           ))}
         </select>
